d42cade8bc897ca57c7c1f057b7ec6aa9d3333d5
Marlon Montenegro <marlonmontenegropaz@gmail.com>
2024-01-01 21:16:58 -0600

    Initial Commit

---
+++ src/main/java/com/montenegro/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.montenegro.springboot.cruddemo.dao;
+
+import com.montenegro.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+
+    List<Employee> findAll();
+
+
+
+}
+
---
+++ src/main/java/com/montenegro/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,33 @@
+package com.montenegro.springboot.cruddemo.dao;
+
+import com.montenegro.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+    //define field for entity manager
+
+    private final EntityManager entityManager;
+
+    //set up constructor injection
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+
+        // create a query
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+        //execute query a get result list
+        //return the results.
+        return theQuery.getResultList();
+    }
+}
